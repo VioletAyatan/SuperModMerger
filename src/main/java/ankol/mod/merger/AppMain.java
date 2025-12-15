4fad55e10148ca9627fabe2023e68edfb64143bd
ankol <2329340232@qq.com>
2025-12-15 10:54:19 +0800

    init

---
+++ src/main/java/ankol/mod/merger/AppMain.java
@@ -0,0 +1,10 @@
+package ankol.mod.merger;
+
+public class AppMain {
+    public static void main(String[] args) {
+        System.out.println("Hello and welcome!");
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+}
