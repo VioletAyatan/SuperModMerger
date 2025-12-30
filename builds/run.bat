a9bd91737b058d36e478b45a35bc92a56dd406de
ankol <ankol@mail.com>
2025-12-30 17:08:59 +0800

    add

---
+++ builds/run.bat
@@ -0,0 +1,7 @@
+rcedit-x64.exe .\SuperModMerger-1.1.0-SNAPSHOT.exe --set-icon .\wukong.ico ^
+--set-file-version 1.1.0 ^
+--set-product-version 1.1.0 ^
+--set-version-string ProductName "Super Mod Merger" ^
+--set-version-string FileDescription "Super Mod Merger" ^
+--set-version-string CompanyName "Ankol" ^
+--set-version-string LegalCopyright "Copyright (C) 2024 Ankol"
