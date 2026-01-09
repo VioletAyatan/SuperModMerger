ac569dca1cf1a1f88b1999b749577df777b06511
ankol <ankol@mail.com>
2026-01-09 16:21:20 +0800

    add

--- src/main/java/ankol/mod/merger/merger/ConflictRecord.kt
+++ src/main/java/ankol/mod/merger/merger/ConflictRecord.kt
@@ -1,48 +1,34 @@
-package ankol.mod.merger.merger;
+package ankol.mod.merger.merger
 
-import ankol.mod.merger.core.BaseTreeNode;
-import lombok.Data;
-import lombok.RequiredArgsConstructor;
+import ankol.mod.merger.core.BaseTreeNode
 
 /**
  * 冲突记录
- *
+ * 
  * @author Ankol
  */
-@Data
-@RequiredArgsConstructor
-public class ConflictRecord {
+data class ConflictRecord(
     /**
      * 冲突的文件名
      */
-    private final String fileName;
+    val fileName: String,
     /**
      * 基础模组名称
      */
-    private final String baseModName;
+    val baseModName: String,
     /**
      * 合并模组名称
      */
-    private final String mergeModName;
-
+    val mergeModName: String,
     /**
      * 冲突的签名
      */
-    private final String signature;
-    private final BaseTreeNode baseNode;
-    private final BaseTreeNode modNode;
+    val signature: String,
+    var baseNode: BaseTreeNode,
+    var modNode: BaseTreeNode,
+
     /**
      * 用户选择
      */
-    private Integer userChoice;
-
-    public ConflictRecord(String fileName, String baseModName, String mergeModName, String signature, BaseTreeNode baseNode, BaseTreeNode modNode, Integer userChoice) {
-        this.fileName = fileName;
-        this.baseModName = baseModName;
-        this.mergeModName = mergeModName;
-        this.signature = signature;
-        this.baseNode = baseNode;
-        this.modNode = modNode;
-        this.userChoice = userChoice;
-    }
-}
+    var userChoice: Int? = null,
+)
